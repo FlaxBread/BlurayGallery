bee2f4d4f610f3bffc6defbb336bb34d020e7024
Erin Laing <elaing01@gmail.com>
2016-04-05 10:43:02 -0400

    Added files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+var imageCount = 1;
+var totalAmount = 4;
+
+function slide(x) 
+{
+	var image = document.getElementById("mainSliderImage");
+	imageCount = imageCount + x;
+	if(imageCount > totalAmount){imageCount = 1;}
+	if(imageCount < 1){imageCount = totalAmount;} //Keeps the 
+	image.src = "images/CoverImage_"+ imageCount +".jpg";
+}
+
+window.addEventListener("load", init, false);
