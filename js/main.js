2f2ebe995f3a06084a8a54331f9c3b593bde4323
Erin Laing <elaing01@gmail.com>
2016-04-05 11:40:08 -0400

    Testing setInterval again

--- js/main.js
+++ js/main.js
@@ -10,9 +10,8 @@ function slide(x)
 	if(imageCount > totalAmount){imageCount = 1;}
 	if(imageCount < 1){imageCount = totalAmount;} //Keeps the 
 	image.src = "images/CoverImage_"+ imageCount +".jpg";
-	setTimeout(slide(1),5000);
 }
 
-slide(1);
+setInterval(slide(),5000,1);
 
 window.addEventListener("load", init, false);
