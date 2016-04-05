25ec57b4cbca55c8bcbf74da566016c1b9c9830e
Erin Laing <elaing01@gmail.com>
2016-04-05 12:26:59 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var imageCount = 1;
 var totalAmount = 4;
 var image = document.getElementById("mainSliderImage");
+var timer = setInterval(slide,5000,1);
 
 function slide(x) 
 {
@@ -10,8 +11,10 @@ function slide(x)
 	if(imageCount > totalAmount){imageCount = 1;}
 	if(imageCount < 1){imageCount = totalAmount;} //Keeps the 
 	image.src = "images/CoverImage_"+ imageCount +".jpg";
+	clearInterval(timer);
+	timer;
 }
 
 
-window.setInterval(slide,5000, 1);
+window.setTimeout(slide,5000, 1);
 window.addEventListener("load", init, false);
