05bf486435dc266891827a6d991b0f3137d437db
Erin Laing <elaing01@gmail.com>
2016-04-05 12:01:42 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,24 @@
 
 var imageCount = 1;
 var totalAmount = 4;
+var image = document.getElementById("mainSliderImage");
 
 function slide(x) 
 {
-	var image = document.getElementById("mainSliderImage");
 	imageCount = imageCount + x;
 	if(imageCount > totalAmount){imageCount = 1;}
 	if(imageCount < 1){imageCount = totalAmount;} //Keeps the 
 	image.src = "images/CoverImage_"+ imageCount +".jpg";
 }
 
-//setInterval(slide(),5000,1);
+function autoSlide(){
+	imageCount ++;
+	if(imageCount > totalAmount){
+		imageCount = 1;
+	}
+	image.src = "images/CoverImage_"+ imageCount +".jpg";
+}
+
+setInterval(autoSlide(),5000);
 
 window.addEventListener("load", init, false);
